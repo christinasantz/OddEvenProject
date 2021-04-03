fb3bcdd056e85405703d0efed6458a55c51ac7c5
christinasantz <christinasantz@gmail.com>
2021-04-03 22:06:22 +0300

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,13 @@
+public class Example {
+    public static void main(String[] args) {
+         int a = 10;
+
+         if ((a % 2)==0){
+             System.out.println("a is even");
+         }
+         else {
+             System.out.println("a is odd");
+         }
+
+    }
+}
